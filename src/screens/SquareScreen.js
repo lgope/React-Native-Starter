b88bf7a23c23e6be3dc2fbe752844d8d61cbcb71
lgope <ilakshmangope@gmail.com>
2020-01-09 02:42:00 +0600

    updated packages 🗽🛠

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -11,6 +11,7 @@ const SquareScreen = () => {
 
   const setColor = (color, change) => {
     //   color === 'red', 'green', 'blue'
+    // change === +15, -15
     switch (color) {
       case 'red':
         red + change > 255 || red + change < 0 ? null : setRed(red + change);
